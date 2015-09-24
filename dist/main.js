4192fdde1bcf3a1c76da9651597c1f737fc5b456
Szymon Piwowarczyk <szymon@piwowarczyk.net>
2015-09-24 12:37:08 +0200

    Possibility to override custom validator getValue method to support non-supported input controls (even not based on standard html inputs)

--- dist/main.js
+++ dist/main.js
@@ -296,13 +296,24 @@ Validator.prototype.getValue = function() {
 CustomValidator.prototype = Object.create(Validator.prototype);
 CustomValidator.prototype.constructor = CustomValidator;
 
-function CustomValidator(name, element, fn) {
+function CustomValidator(name, element, fn, valueFn) {
 	Validator.call(this, name, element);
 	this.fn = fn;
+	this.valueFn = valueFn;
 }
 
 CustomValidator.prototype.validate = function() {
 	return this.fn(this.getValue());
+};
+
+CustomValidator.prototype.getValue = function() {
+	var fn;
+	if(typeof this.valueFn === 'undefined') {
+		fn = Validator.prototype.getValue.apply(this, arguments);
+	} else {
+		fn = this.valueFn.apply(this, arguments);
+	}
+	return fn;
 };
 
 RegexpValidator.prototype = Object.create(Validator.prototype);
--- js/validators/custom.js
+++ js/validators/custom.js
@@ -1,11 +1,22 @@
 CustomValidator.prototype = Object.create(Validator.prototype);
 CustomValidator.prototype.constructor = CustomValidator;
 
-function CustomValidator(name, element, fn) {
+function CustomValidator(name, element, fn, valueFn) {
 	Validator.call(this, name, element);
 	this.fn = fn;
+	this.valueFn = valueFn;
 }
 
 CustomValidator.prototype.validate = function() {
 	return this.fn(this.getValue());
+};
+
+CustomValidator.prototype.getValue = function() {
+	var fn;
+	if(typeof this.valueFn === 'undefined') {
+		fn = Validator.prototype.getValue.apply(this, arguments);
+	} else {
+		fn = this.valueFn.apply(this, arguments);
+	}
+	return fn;
 };
